f6530830b16a344146c6ec783f601155555d99c6
jorge <jorge.f@velosoft.mx>
2017-04-27 16:35:02 -0500

    Nuevo slider

--- quienes-somos.php
+++ quienes-somos.php
@@ -1,69 +1,210 @@
 
+
+<div class="bg-equipo23">
+
 <?php
   require("functions.php");
   get_header();
 ?>
+<link type="text/css" rel="stylesheet" href="css/slick1.css" />
+<link type="text/css" rel="stylesheet" href="css/slick-theme1.css" />
+<link rel="stylesheet" type="text/css" href="css/slick-style1.css">
+
+<style>
+@media (max-width: 992px){
+  .center {
+    margin-right: 0% !important;
+    margin-left: 0% !important;
+  }
+  .slick-next {
+    right: -15px;
+  }
+  .slick-prev {
+    left: 0px;
+  }
+}
+@media only screen and (min-width:992px) and (max-width:1200px){
+  .valign-wrapper {
+    height: auto;
+  }
+}
+
+</style>
+
 
 <div class="animsition"
-  data-animsition-in-class="fade-in-left-sm"
+  data-animsition-in-class="fade-in-up-sm"
   data-animsition-in-duration="1000"
-  data-animsition-out-class="fade-out-left-sm"
-  data-animsition-out-duration="800">
-
-  <div class="row nomargin bg-quienes">
-    <div class="valign-wrapper">
-      <div class="col l12 s12 valign">
-        <div class="col l6 m6 offset-m3 s12 center imagen-somos">
-          <img src="images/recursos/quienes-somos1.png" title="Quiénes Somos" alt="Legal Nest">
-        </div>
-        <div class="col l6 m10 offset-m1 s12 top-100">
-          <div class="col l2 m4 s3 top-50 imagen-somos">
-            <div class="col l12 m6 offset-m3 s12">
-              <img src="images/recursos/one.png" title="Quiénos Somos uno" alt="Legal Nest">
+  data-animsition-out-class="fade-out-up-sm">
+<div class="row nomargin bg-quienes7">
+  <div class="valign-wrapper">
+    <div class="col l12 m12 s12 valign">
+      <div class="col l10 offset-l1 m12 s12 spacing">
+        <div class="slider center">
+          <div>
+            <div class="col l5 offset-l1 m6 offset-m3 s12">
+              <div class="col l12 s12 offset imagen-somos">
+                <img src="images/recursos/quienes-somos1.png" title="Quiénes Somos" alt="Legal Nest">
+              </div>
+            </div>
+            <div class="col l6 m10 offset-m1 s12 testimonio1 top1-100">
+              <div class="col l2 m4 s3 top-50 imagen-somos">
+                <div class="col l12 m6 offset-m3 s12">
+                  <img src="images/recursos/one.png" title="Quiénos Somos uno" alt="Legal Nest">
+                </div>
+              </div>
+              <div class="col l10 m8 s9">
+                <div class="col l12 s12 quienes-1">
+                  <p class="nomargin"><strong>nuestra</strong> misión</p>
+                </div>
+                <div class="col l12 s12 quienes-2">
+                  <p class="nomargin">es encontrar</p>
+                  <p class="nomargin">soluciones a tu</p>
+                  <p class="nomargin"><strong>negocio</strong></p>
+                </div>
+              </div>
             </div>
           </div>
-          <div class="col l10 m8 s9">
-            <div class="col l12 s12 quienes-1">
-              <p class="nomargin"><strong>nuestra</strong> misión</p>
+          <div>
+            <div class="col l5 offset-l1 m6 offset-m3 s12">
+              <div class="col l12 s12 offset imagen-somos">
+                <img src="images/recursos/quienes-somos2.png" title="Quiénes Somos" alt="Legal Nest">
+              </div>
             </div>
-            <div class="col l12 s12 quienes-2">
-              <p class="nomargin">es encontrar</p>
-              <p class="nomargin">soluciones a tu</p>
-              <p class="nomargin"><strong>negocio</strong></p>
+            <div class="col l6 m10 offset-m1 s12 testimonio1 top-100">
+              <div class="col l2 m4 s3 top-50 imagen-somos">
+                <div class="col l12 m6 offset-m3 s12">
+                  <img src="images/recursos/2.png" title="Quiénos Somos uno" alt="Legal Nest">
+                </div>
+              </div>
+              <div class="col l10 m8 s9">
+                <div class="col l12 s12 quienes-3">
+                  <p class="nomargin"><strong>la conultoría</strong></p>
+                  <p class="nomargin"><strong>legal</strong> debería</p>
+                </div>
+                <div class="col l12 s12 quienes-4">
+                  <p class="nomargin">de ser una</p>
+                  <p class="nomargin"><strong>solución</strong>,</p>
+                </div>
+                <div class="col l12 s12 quienes-5">
+                  <p class="nomargin"><strong>no</strong> un problema</p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col l12 s12 top-50">
-          <div class="col l4 offset-l4 s12 center">
-            <a href="quienes-somos2.php" class="animsition-link"><img width="50px" src="images/recursos/next.png" alt="Legal Nest"></a>
+          <div>
+            <div class="col l5 offset-l1 m6 offset-m3 s12">
+              <div class="col l12 s12 offset imagen-somos">
+                <img src="images/recursos/quienes-somos3.png" title="Quiénes Somos" alt="Legal Nest">
+              </div>
+            </div>
+            <div class="col l6 m10 offset-m1 s12 testimonio1 top-100">
+              <div class="col l2 m4 s3 top-50 imagen-somos">
+                <div class="col l12 m6 offset-m3 s12">
+                  <img src="images/recursos/3.png" title="Quiénos Somos uno" alt="Legal Nest">
+                </div>
+              </div>
+              <div class="col l10 m8 s9">
+                <div class="col l12 s12 quienes-1">
+                  <p class="nomargin"><strong>costos</strong> claros</p>
+                </div>
+                <div class="col l12 s12 quienes-6">
+                  <p class="nomargin">para una</p>
+                </div>
+                <div class="col l12 s12 quienes-5">
+                  <p class="nomargin">relación larga</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="col l5 offset-l1 m6 offset-m3 s12">
+              <div class="col l12 s12 offset imagen-somos">
+                <img src="images/recursos/quienes-somos4.png" title="Quiénes Somos" alt="Legal Nest">
+              </div>
+            </div>
+            <div class="col l6 m10 offset-m1 s12 testimonio1 top-100">
+              <div class="col l2 m4 s3 top-50 imagen-somos">
+                <div class="col l12 m6 offset-m3 s12">
+                  <img src="images/recursos/4.png" title="Quiénos Somos uno" alt="Legal Nest">
+                </div>
+              </div>
+              <div class="col l10 m8 s9">
+                <div class="col l12 s12 quienes-1">
+                  <p class="nomargin"><strong>cobramos por</strong></p>
+                </div>
+                <div class="col l12 s12 quienes-6">
+                  <p class="nomargin">ejecución</p>
+                </div>
+                <div class="col l12 s12 quienes-5">
+                  <p class="nomargin">no <strong>por hora</strong></p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 
 
+</div>
 
 <?php
   get_footer();
 ?>
 
+<script type="text/javascript" src="js/slick.js"></script>
+
 </div>
 
 <script>
-  window.onload=function(){
-    setTimeout(function() {
-      console.log("lupita")
-      location.href="quienes-somos2.php";
-    },7000);
+
+$('.center').slick({
+// dots: true,
+centerMode: true,
+autoplay:true,
+centerPadding: '0px',
+slidesToShow: 1,
+responsive: [
+{
+  breakpoint: 992,
+  settings: {
+    arrows: true,
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 1
   }
+},
+{
+  breakpoint: 768,
+  settings: {
+    arrows: true,
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 1
+  }
+},
+{
+  breakpoint: 480,
+  settings: {
+    arrows: true,
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 1
+  }
+}
+]
+});
 </script>
+
 
 <script>
 $(document).ready(function() {
   $(".animsition").animsition({
-    inClass: 'fade-in-left-sm',
-    outClass: 'fade-out-left-sm',
+    inClass: 'fade-in-up-sm',
+    outClass: 'fade-out-up-sm',
     inDuration: 1500,
     outDuration: 800,
     linkElement: '.animsition-link',
